88dedd4fa77c137dcf857db8fc2a52fa1577c3f0
Andrii Ferenchuk <ferenchuk.a@gmail.com>
2025-01-05 23:42:42 +0200

    add buttons to cart

--- js/cart.js
+++ js/cart.js
@@ -1,12 +1,15 @@
+import { getSelectedObjOfElement } from "./product.js"
+
 const cartProductListContainer = document.querySelector(
   ".cart-product-list-container"
 )
 
-import { getSelectedObjOfElement } from "./product.js"
 const renderProductList = function (cart) {
   cartProductListContainer.innerHTML = ""
+  let total = 0
   Object.keys(cart).forEach((id) => {
     getSelectedObjOfElement(id).then((obj) => {
+      total += parseInt(obj.price).toFixed(2) * cart[id]
       cartProductListContainer.innerHTML += `
     <div class="cart-product">
         <div class="cart-product-img-container container container-fluid">
@@ -22,15 +25,25 @@ const renderProductList = function (cart) {
                 )}</h6>
             </div>
             <div class="cart-product-info-quantity container container-fluid">
-                <p class="cart-product-quantity">Кількість: ${cart[id]}</p>
+                <p class="cart-product-quantity">Кількість: ${cart[id]} (${
+        cart[id]
+      } * ${parseInt(obj.price).toFixed(2)})</p>
             </div>
             <div class="cart-product-info-price container container-fluid">
-                <p class="cart-product-price">Ціна: ${parseInt(
-                  obj.price
-                ).toFixed(2)} грн</p>
+                <p class="cart-product-price">Сума: ${(
+                  parseInt(obj.price) * cart[id]
+                ).toFixed(2)}  грн</p>
             </div>
         </div>
+        <div class='product-quantity-modificators'>            
+            <button data-id="${id}" class='btn btn-light btn-sm deduct-product_cart'>-</button>
+            <button data-id="${id}" class='btn btn-light btn-sm remove-product_cart'>x</button>
+            <button data-id="${id}" class='btn btn-light btn-sm add-product_cart'>+</button>
+        </div>
     </div>
+    `
+      document.querySelector(".total-container").innerHTML = `
+    <p class="total">Загальна сума: ${total.toFixed(2)} грн</p>
     `
     })
   })
